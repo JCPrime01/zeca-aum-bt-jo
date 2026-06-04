e174acc0e34d9238ada8e007f9aaed897500357b
JCPrime01 <vanzokiller@gmail.com>
2026-06-04 10:06:36 -0300

    chore: shareCode W98BGVU6SPC -> XSO69UBI4BW (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/2wo6uo64?shareCode=W98BGVU6SPC&afp8=oa&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/2wo6uo64?shareCode=XSO69UBI4BW&afp8=oa&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
