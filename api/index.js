12f00f13b80a667da4b2096383bf4a15905bdd93
JCPrime01 <vanzokiller@gmail.com>
2026-06-30 09:41:46 -0300

    chore: shareCode KCC13GZFPD6 -> BLW6DG1ICKC (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23426&shareCode=KCC13GZFPD6&afp2=oa&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23426&shareCode=BLW6DG1ICKC&afp2=oa&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
